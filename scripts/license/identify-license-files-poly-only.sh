a7842b6ef17b665a3721d959033ffbc9e9d41c13
Seán Dunne <sean.dunne@harness.io>
2022-01-17 17:01:29 +0000

    feat: [ONP-258]: handle multiple filepaths in new file stamping script

--- scripts/license/identify-license-files-poly-only.sh
+++ scripts/license/identify-license-files-poly-only.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# Copyright 2021 Harness Inc. All rights reserved.
-# Use of this source code is governed by the PolyForm Free Trial 1.0.0 license
+# Copyright 2022 Harness Inc. All rights reserved.
+# Use of this source code is governed by the PolyForm Shield 1.0.0 license
 # that can be found in the licenses directory at the root of this repository, also available at
-# https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
-#
+# https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt.
+
 # Produces source files intended for use with add_license_header.sh
 # It produces a file containing the source files which should be stamped with the license header.
 #   sources-all.txt
